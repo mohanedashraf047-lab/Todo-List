146a756b228036067906fed3ca1feaf77bedd747
Mohand Ashraf <mohanedashraf047@gmail.com>
2025-10-30 18:37:33 +0300

    title-img

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -5,7 +5,7 @@ export default function Title() {
     <div className="title">
       <h1>Todo List</h1>
       <img
-        src="../../public/icons8-to-do-list-windows-11-color-96.png"
+        src="../assets/icons8-to-do-list-windows-11-color-96.png"
         alt="Notebook"
         srcSet=""
       />
