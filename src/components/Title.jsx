a4517b7377bd1723f8183e36b6d1a9798b3bfbe4
Mohand Ashraf <mohanedashraf047@gmail.com>
2025-10-30 18:45:53 +0300

    imge

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import titleImage from "../assets/todo-list3.png";
 
 export default function Title() {
   return (
     <div className="title">
       <h1>Todo List</h1>
       <img
-        src="../assets/todo-list3.png"
+        src={titleImage}
         alt="Notebook"
         srcSet=""
       />
